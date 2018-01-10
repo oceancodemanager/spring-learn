3ff1b312a0d189f1f219e6c93ad09765a26a96d0
ocean <ocean@DESKTOP-47HN69M>
2018-01-10 18:04:53 +0800

    有趣的测试

--- src/test/java/com/ocean/learn/spring/test/thread/Executor.java
+++ src/test/java/com/ocean/learn/spring/test/thread/Executor.java
@@ -6,6 +6,11 @@ import java.util.concurrent.Executors;
 
 public class Executor {
 	public static volatile int a = 0;
+	// 有趣的测试
+	/**
+	 * newSingleThreadExecutor的情况下，如果不发生错误就是一个线程在执行
+	 * 
+	 */
 	// private static final ExecutorService executorService =
 	// Executors.newSingleThreadExecutor();
 	private static final ExecutorService executorService = Executors.newFixedThreadPool(20);
