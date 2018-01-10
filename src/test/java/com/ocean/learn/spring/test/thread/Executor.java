fdd15afb6b29db6203291f4101305e053a5bc204
ocean <ocean@DESKTOP-47HN69M>
2018-01-10 18:02:52 +0800

    Executor

---
+++ src/test/java/com/ocean/learn/spring/test/thread/Executor.java
@@ -0,0 +1,34 @@
+package com.ocean.learn.spring.test.thread;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Executor {
+	public static volatile int a = 0;
+	// private static final ExecutorService executorService =
+	// Executors.newSingleThreadExecutor();
+	private static final ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			executorService.execute(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("thread_name:" + Thread.currentThread().getName());
+					if (new Random().nextBoolean()) {
+						// String a = null;
+						// System.out.println(a.length());
+					}
+					try {
+						Thread.sleep(100000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					// Executor.a++;
+					// System.out.println("a" + Executor.a++);
+				}
+			});
+		}
+	}
+}
